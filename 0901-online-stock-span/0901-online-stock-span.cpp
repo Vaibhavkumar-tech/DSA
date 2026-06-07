041c5ef0b90912624d78d550ff23eabfee8a7adf
Vaibhav Kumar <vaibhavkumar62078@gmail.com>
2026-06-07 16:04:38 +0530

    Time: 34 ms (45.12%), Space: 92 MB (58.04%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -0,0 +1,32 @@
+class StockSpanner {
+public:
+
+    stack<pair<int,int>> st;
+    int idx;
+
+    StockSpanner() {
+        idx = -1;
+    }
+
+    int next(int price) {
+
+        idx++;
+
+        while(!st.empty() && price >= st.top().first) {
+            st.pop();
+        }
+
+        int ans;
+
+        if(st.empty()) {
+            ans = idx + 1;
+        }
+        else {
+            ans = idx - st.top().second;
+        }
+
+        st.push({price, idx});
+
+        return ans;
+    }
+};
