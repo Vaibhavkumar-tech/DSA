a2054a59c01dec67e11c2075b5479684422c2dac
Vaibhav Kumar <vaibhavkumar62078@gmail.com>
2026-06-08 19:09:01 +0530

    Time: 0 ms (100%), Space: 8.2 MB (45.94%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -2,34 +2,26 @@ class Solution {
 public:
     bool checkValidString(string s) {
         int n=s.size();
-        stack<int>open;
-        stack<int>star;
+        int mino=0;
+        int maxi=0;
         for(int i=0;i<n;i++){
             if(s[i]=='('){
-                open.push(i);
+                mino++;
+                maxi++;
             }
-            else if(s[i]=='*'){
-                star.push(i);
+            else if(s[i]==')'){
+                mino--;
+                maxi--;
             }
             else{
-                if(!open.empty()){
-                    open.pop();
-                }
-                else if(!star.empty()){
-                    star.pop();
-                }
-                else{
-                    return false;
-                }
+                mino--;
+                maxi++;
             }
-        }
-        while(!open.empty() && !star.empty()){
-            if(open.top()>star.top()){  //matlab * cannot be work close bracket
-               return false;
+            if(maxi<0){
+                return false;
             }
-            star.pop();
-            open.pop();
+            mino=max(mino,0);
         }
-        return open.empty();
+        return mino == 0;
     }
 };
