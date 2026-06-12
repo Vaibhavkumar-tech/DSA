21b37422f2d409a377990bd8cfb675cd2ce12343
Vaibhav Kumar <vaibhavkumar62078@gmail.com>
2026-06-12 14:41:37 +0530

    Time: 2 ms (63.39%), Space: 120.3 MB (49.85%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -8,54 +8,38 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
-
     ListNode* reverse(ListNode* head){
-
-        if(head == NULL || head->next == NULL){
+        if(head==NULL || head->next==NULL){
             return head;
         }
-
-        ListNode* newHead = reverse(head->next);
-
-        ListNode* front = head->next;
-
-        front->next = head;
-
-        head->next = NULL;
-
+        ListNode* newHead=reverse(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=NULL;
         return newHead;
     }
-
     bool isPalindrome(ListNode* head) {
+        vector<int>ans;
+        ListNode* fast=head;
+        ListNode* slow=head;
 
-        ListNode* fast = head;
-        ListNode* slow = head;
-
-        // even mein 2 hota mid then mid2 chahiye
-        while(fast->next != NULL && fast->next->next != NULL){
-
-            fast = fast->next->next;
-            slow = slow->next;
+        //even mein 2 hota mid then mid2 chahiye
+        while(fast->next!=NULL && fast->next->next!=NULL){
+            fast=fast->next->next;
+            slow=slow->next;
         }
-
-        // mid se reverse kr do means half-half kr diye then dono same hona chahiye
-        ListNode* tail = reverse(slow->next);
-
-        ListNode* temp = head;
-
-        while(tail != NULL){
-
-            if(temp->val != tail->val){
+        //mid se reverse kr do means half-half kr diye then dono same hona chahiye
+        ListNode* tail=reverse(slow->next);
+        ListNode* temp=head;
+        while(tail!=NULL){
+            if(temp->val!=tail->val){
                 return false;
             }
-
-            temp = temp->next;
-            tail = tail->next;
+            temp=temp->next;
+            tail=tail->next;
         }
-
         return true;
     }
 };
