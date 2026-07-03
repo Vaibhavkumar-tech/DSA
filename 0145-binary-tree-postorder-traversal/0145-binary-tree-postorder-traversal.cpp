4d81539a2093664f449de21d2e7bb7b2dbcbba46
Vaibhav Kumar <vaibhavkumar62078@gmail.com>
2026-07-03 10:31:52 +0530

    Time: 0 ms (100%), Space: 11.2 MB (7.28%) - LeetHub

---
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int>ans;
+    void Solve(TreeNode* root){
+        if(root==NULL) return;
+        Solve(root->left);
+        Solve(root->right);
+        ans.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        Solve(root);
+        return ans;
+    }
+};
